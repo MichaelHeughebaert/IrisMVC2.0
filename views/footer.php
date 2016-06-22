f2b4b6719ea773215c12301546300675b0a7bcf4
root <root>
2016-06-22 11:29:08 +0200

    Added initial footer & header

---
+++ views/footer.php
@@ -0,0 +1,21 @@
+        </div>
+    </div>
+</div>
+
+<script src="<?= URL; ?>public/global/plugins/jquery.min.js" type="text/javascript"></script>
+<script src="<?= URL; ?>public/global/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="<?= URL; ?>public/global/plugins/js.cookie.min.js" type="text/javascript"></script>
+<script src="<?= URL; ?>public/global/plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js"
+    type="text/javascript"></script>
+<script src="<?= URL; ?>public/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js"
+    type="text/javascript"></script>
+<script src="<?= URL; ?>public/global/plugins/jquery.blockui.min.js" type="text/javascript"></script>
+<script src="<?= URL; ?>public/global/plugins/uniform/jquery.uniform.min.js" type="text/javascript"></script>
+<script src="<?= URL; ?>public/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js"
+    type="text/javascript"></script>
+<script src="<?= URL; ?>public/global/scripts/app.min.js" type="text/javascript"></script>
+<script src="<?= URL; ?>public/global/scripts/layout.min.js" type="text/javascript"></script>
+<script src="<?= URL; ?>public/global/scripts/demo.min.js" type="text/javascript"></script>
+<script src="<?= URL; ?>public/global/scripts/quick-sidebar.min.js" type="text/javascript"></script>
+</body>
+</html>
---
+++ views/header.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <title><?= (isset($this->title)) ? 'Intranet | ' . $this->title : 'Intranet'; ?></title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta content="width=device-width, initial-scale=1" name="viewport"/>
+    <meta content="Politiezone Oostende intranet" name="description"/>
+    <meta content="Dienst ICT" name="author"/>
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet"
+          type="text/css"/>
+    <link href="<?= URL; ?>public/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet"
+          type="text/css"/>
+    <link href="<?= URL; ?>public/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet"
+          type="text/css"/>
+    <link href="<?= URL; ?>public/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+    <link href="<?= URL; ?>public/global/plugins/uniform/css/uniform.default.css" rel="stylesheet" type="text/css"/>
+    <link href="<?= URL; ?>public/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet"
+          type="text/css"/>
+    <link href="<?= URL; ?>public/global/css/components.min.css" rel="stylesheet" id="style_components"
+          type="text/css"/>
+    <link href="<?= URL; ?>public/global/css/plugins.min.css" rel="stylesheet" type="text/css"/>
+    <link href="<?= URL; ?>public/global/css/layout.min.css" rel="stylesheet" type="text/css"/>
+    <link href="<?= URL; ?>public/global/css/themes/darkblue.min.css" rel="stylesheet" type="text/css"
+          id="style_color"/>
+    <link href="<?= URL; ?>public/global/css/custom.min.css" rel="stylesheet" type="text/css"/>
+    <link rel="shortcut icon" href="<?= URL; ?>public/global/img/favicon.ico"/>
+</head>
+<body class="page-header-fixed page-sidebar-closed-hide-logo page-container-bg-solid page-content-white">
+<div class="page-header navbar navbar-fixed-top">
+    <div class="page-header-inner ">
+        <div class="page-logo">
+            <a href="<?= URL; ?>dashboard"><img src="<?= URL; ?>public/global/img/logo.png" alt="logo"
+                                                class="logo-default"/></a>
+            <div class="menu-toggler sidebar-toggler"></div>
+        </div>
+    </div>
+    <a href="javascript:;" class="menu-toggler responsive-toggler" data-toggle="collapse"
+       data-target=".navbar-collapse"> </a>
+    <div class="top-menu">
+        <ul class="nav navbar-nav pull-right">
+            <li class="dropdown dropdown-user">
+
+            </li>
+        </ul>
+    </div>
+</div>
+<div class="clearfix"></div>
+<div class="page-container">
+    <div class="page-sidebar-wrapper">
+        <div class="page-sidebar navbar-collapse collapse">
+            <ul class="page-sidebar-menu  page-header-fixed " data-keep-expanded="false" data-auto-scroll="true"
+                data-slide-speed="200" style="padding-top: 20px">
+                <li class="sidebar-toggler-wrapper hide">
+                    <div class="sidebar-toggler"></div>
+                </li>
+                <li class="sidebar-search-wrapper">
+                    <form class="sidebar-search  sidebar-search-bordered" method="POST">
+                        <a href="javascript:;" class="remove"><i class="icon-close"></i></a>
+                        <div class="input-group">
+                            <input type="text" class="form-control" placeholder="Search...">
+                                <span class="input-group-btn">
+                                    <a href="javascript:;" class="btn submit">
+                                        <i class="icon-magnifier"></i>
+                                    </a>
+                                </span>
+                        </div>
+                    </form>
+                </li>
+                <li class="nav-item start ">
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="page-content-wrapper">
+        <div class="page-content">
