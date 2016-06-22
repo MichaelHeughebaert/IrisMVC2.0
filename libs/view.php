04b5e21809bf0120fb5c31d4e31aca2dcd9c8e2d
root <root>
2016-06-22 11:29:44 +0200

    Added PHPDoc & render functionality

--- libs/view.php
+++ libs/view.php
@@ -4,5 +4,20 @@ namespace libs;
 
 class View
 {
+    /**
+     * View constructor.
+     */
     public function __construct() {}
+
+    /**
+     * Function used to render a specific view
+     *
+     * @param $view string Contains the name of the view it needs to load.
+     */
+    public function render($view)
+    {
+        require 'views/header.php';
+        require 'views/' . $view . '.php';
+        require 'views/footer.php';
+    }
 }
