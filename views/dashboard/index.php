c9dd9bc4a9b971c03e336a28a507d0c1bf2135fd
root <root>
2016-06-22 11:34:37 +0200

    Current default homepage

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -1,3 +1,14 @@
-<?php
-
-echo 'Dit is de dashboard index view';
+<div class="page-bar">
+    <ul class="page-breadcrumb">
+        <li>
+            <i class="icon-home"></i>
+            <a href="<?= URL; ?>dashboard">Beginpagina</a>
+        </li>
+    </ul>
+</div>
+<h3 class="page-title"> Dashboard
+    <small>this is the index page</small>
+</h3>
+<div class="note note-info">
+    <p> Welcome on the index page </p>
+</div>
