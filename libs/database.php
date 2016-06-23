97b7d9aac2b2b6f678b7c246ab36471c7f426080
root <root>
2016-06-23 13:44:28 +0200

    add doc

--- libs/database.php
+++ libs/database.php
@@ -4,6 +4,17 @@ namespace libs;
 
 class Database extends \PDO
 {
+    /**
+     * Database constructor.
+     *
+     * Connect to database.
+     *
+     * @param string $db_type
+     * @param string $db_host
+     * @param string $db_name
+     * @param string $db_user
+     * @param string $db_pwd
+     */
     public function __construct($db_type, $db_host, $db_name, $db_user, $db_pwd)
     {
         parent::__construct($db_type . ':host=' . $db_host . ';dbname=' . $db_name, $db_user, $db_pwd);
